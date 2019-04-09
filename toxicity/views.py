8cb0e9e68264c2ce5254e0f022b4dfc44684cdb9
ArqumShaikh <arqum212@gmail.com>
2019-04-09 15:43:29 +0530

    Prediction file, model, api and Tesseract code added

--- toxicity/views.py
+++ toxicity/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Images
+import cv2
+import sys
+import pytesseract
 # Create your views here.
 
 def index(request):
@@ -10,4 +13,14 @@ def index(request):
         )
         image.save()
         images = request.FILES.get('image')
+        text = tesseract(images)
+        
     return render(request,"toxicity/index.html")
+
+def tesseract(images):
+    config = ('-l eng --oem 1 --psm 3')
+    path = "./media/images/"+str(images)
+    images = path
+    im = cv2.imread(images,cv2.IMREAD_COLOR)
+    text = pytesseract.image_to_string(im,config=config)
+    return  text
